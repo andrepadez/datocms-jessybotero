2afc4e960caff2f0e4bb3e7deca8eec6b0239464
André Padez <andre.padez@gmail.com>
2019-11-29 21:20:55 -0500

    staging complete

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,28 +4,28 @@ import { HelmetDatoCms } from 'gatsby-source-datocms'
 import Img from 'gatsby-image'
 import Layout from '../components/layout'
 
-const About = () => <h1>en contruccion...</h1>
+// const About = () => <h1>en contruccion...</h1>
 
-// const About = ({ data: { about } }) => (
-//   <Layout>
-//     <article className="sheet">
-//       <HelmetDatoCms seo={about.seoMetaTags} />
-//       <div className="sheet__inner">
-//         <h1 className="sheet__title">{about.title}</h1>
-//         <p className="sheet__lead">{about.subtitle}</p>
-//         <div className="sheet__gallery">
-//           <Img fluid={about.photo.fluid} />
-//         </div>
-//         <div
-//           className="sheet__body"
-//           dangerouslySetInnerHTML={{
-//             __html: about.bioNode.childMarkdownRemark.html,
-//           }}
-//         />
-//       </div>
-//     </article>
-//   </Layout>
-// )
+const About = ({ data: { about } }) => (
+  <Layout>
+    <article className="sheet">
+      <HelmetDatoCms seo={about.seoMetaTags} />
+      <div className="sheet__inner">
+        <h1 className="sheet__title">{about.title}</h1>
+        <p className="sheet__lead">{about.subtitle}</p>
+        <div className="sheet__gallery">
+          <Img fluid={about.photo.fluid} />
+        </div>
+        <div
+          className="sheet__body"
+          dangerouslySetInnerHTML={{
+            __html: about.bioNode.childMarkdownRemark.html
+          }}
+        />
+      </div>
+    </article>
+  </Layout>
+)
 
 export default About
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,31 +4,31 @@ import Masonry from 'react-masonry-component'
 import Img from 'gatsby-image'
 import Layout from '../components/layout'
 
-const IndexPage = () => <h1>en contruccion...</h1>
+// const IndexPage = () => <h1>en contruccion...</h1>
 
-// const IndexPage = ({ data }) => (
-//   <Layout>
-//     <Masonry className="showcase">
-//       {data.allDatoCmsWork.edges.map(({ node: work }) => (
-//         <div key={work.id} className="showcase__item">
-//           <figure className="card">
-//             <Link to={`/works/${work.slug}`} className="card__image">
-//               <Img fluid={work.coverImage.fluid} />
-//             </Link>
-//             <figcaption className="card__caption">
-//               <h6 className="card__title">
-//                 <Link to={`/works/${work.slug}`}>{work.title}</Link>
-//               </h6>
-//               <div className="card__description">
-//                 <p>{work.excerpt}</p>
-//               </div>
-//             </figcaption>
-//           </figure>
-//         </div>
-//       ))}
-//     </Masonry>
-//   </Layout>
-// )
+const IndexPage = ({ data }) => (
+  <Layout>
+    <Masonry className="showcase">
+      {data.allDatoCmsWork.edges.map(({ node: work }) => (
+        <div key={work.id} className="showcase__item">
+          <figure className="card">
+            <Link to={`/works/${work.slug}`} className="card__image">
+              <Img fluid={work.coverImage.fluid} />
+            </Link>
+            <figcaption className="card__caption">
+              <h6 className="card__title">
+                <Link to={`/works/${work.slug}`}>{work.title}</Link>
+              </h6>
+              <div className="card__description">
+                <p>{work.excerpt}</p>
+              </div>
+            </figcaption>
+          </figure>
+        </div>
+      ))}
+    </Masonry>
+  </Layout>
+)
 
 export default IndexPage
 
